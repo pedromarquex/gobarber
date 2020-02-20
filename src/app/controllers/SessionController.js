ad9692260a88ba5201da315c875b8927f956c634
Pedro Marques <junnior.pedro.pm@gmail.com>
2020-02-20 02:05:58 -0300

    added user update and input data validation

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,21 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -13,7 +24,7 @@ class SessionController {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    if (!(await user.checkPassword(password)) ) {
+    if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
