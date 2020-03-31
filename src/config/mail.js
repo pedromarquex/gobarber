216dbfd1376289a8aa872b9e3f2aa66eea9312eb
Pedro Marques <junnior.pedro.pm@gmail.com>
2020-03-31 01:50:58 -0300

    added nodemailer service to notify appointment cancellation

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f770174769d526',
+    pass: '744d2280e16951',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+}
